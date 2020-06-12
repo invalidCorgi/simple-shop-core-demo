853945e5d7db8182b16edb81c4f4f7320a01eb9a
invalidcorgi <invalidcorgi@gmail.com>
2020-06-12 17:47:37 +0200

    Changed main page to products

--- EcommSimpleShop/Startup.cs
+++ EcommSimpleShop/Startup.cs
@@ -70,7 +70,7 @@ namespace EcommSimpleShop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Product}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
